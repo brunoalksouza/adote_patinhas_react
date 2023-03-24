92fc6e4520d4ce9b84025f58e6423e320627cdbe
Hideki Kishi <jp.hidekifk@gmail.com>
2023-03-24 10:32:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Footer from "./components/Footer/footer";
 
 function App() {
   return (
-    <div className="App">
-      <Filter />;
+    <div>
+      <Filter />
       <Footer />
     </div>
     
