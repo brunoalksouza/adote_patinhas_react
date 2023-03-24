0504d667b8e3bdb0d36d3d7104767a8e94468413
Hideki Kishi <jp.hidekifk@gmail.com>
2023-03-24 10:29:01 -0300

    Footer
    acrescentei o footer, adicionei a fonte e alterei a font-size do filtro para 18

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import Filter from "./components/Filter/Filter";
+import Footer from "./components/Footer/footer";
 
 function App() {
-  return <Filter />;
+  return (
+    <div className="App">
+      <Filter />;
+      <Footer />
+    </div>
+    
+  )
 }
 
 export default App;
