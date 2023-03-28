75e657d6dbcab1e98cd5c6542651aef516b694f9
Karina Martins <102524771+karinasm9@users.noreply.github.com>
2023-03-28 11:01:08 -0300

    componente - botão

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import "./App.css";
-import Filter from "./components/Filter/Filter";
+import ButtonAdd from "./componentes/ButtonAdd";
+import ButtonDelete from "./componentes/ButtonDelete";
+import ButtonDetails from "./componentes/ButtonDetails";
+import ButtonFace from "./componentes/ButtonFace";
+import ButtonGoogle from "./componentes/ButtonGoogle";
+import ButtonInterest from "./componentes/ButtonInterest";
+import ButtonRegister from "./componentes/ButtonRegister";
+import ButtonRemove from "./componentes/ButtonRemove";
 
 function App() {
-  return <Filter />;
+  return (
+    <div className="App">
+      <ButtonInterest/>
+      <ButtonGoogle/>
+      <ButtonFace/>
+      <ButtonDetails/>
+      <ButtonRegister/>
+      <ButtonAdd/>
+      <ButtonDelete/>
+      <ButtonRemove/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/componentes/ButtonInterest/index.js
@@ -0,0 +1,17 @@
+
+import ButtonInterest from "../Button";
+
+import "./Card.css"
+
+const CardInterest = () => {
+    return (
+        <section className="cardInterest">
+            <h2>Interesse</h2>
+            <div>
+                <ButtonInterest corFundo='#516953'>TENHO INTERESSE</ButtonInterest>
+            </div>
+        </section>
+    )
+}
+
+export default CardInterest;
